9467aed58e04cb576cb405b10f3073d11a113f57
Roman Karasev <progressivenation12@gmail.com>
2024-04-02 21:59:42 +0300

    add new file Dog (is linked by name to AnimalCage via Animal), modified files AnimalsCage, Application,Cat, Timer, MainTest

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,15 +6,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+                    context.getBean("animalsCage", AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
-
 }
